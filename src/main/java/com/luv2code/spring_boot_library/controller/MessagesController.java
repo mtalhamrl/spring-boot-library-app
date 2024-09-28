18ec7ee6b298bfc2eae8e04b5ac236c206001cbe
mtalhamrl <110231439+mtalhamrl@users.noreply.github.com>
2024-09-28 12:21:53 +0300

    create admin message request and set backend for give answer user questions

--- src/main/java/com/luv2code/spring_boot_library/controller/MessagesController.java
+++ src/main/java/com/luv2code/spring_boot_library/controller/MessagesController.java
@@ -1,6 +1,7 @@
 package com.luv2code.spring_boot_library.controller;
 
 import com.luv2code.spring_boot_library.entity.Message;
+import com.luv2code.spring_boot_library.requestmodels.AdminQuestionRequest;
 import com.luv2code.spring_boot_library.service.MessagesService;
 import com.luv2code.spring_boot_library.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,17 @@ public class MessagesController {
                             @RequestBody Message messageRequest) {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messagesService.postMessage(messageRequest, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception{
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if(admin == null || !admin.equals("admin")){
+            throw new Exception("Admin page only");
+        }
+        messagesService.putMessage(adminQuestionRequest,userEmail);
     }
 
 }
--- src/main/java/com/luv2code/spring_boot_library/service/MessagesService.java
+++ src/main/java/com/luv2code/spring_boot_library/service/MessagesService.java
@@ -2,9 +2,12 @@ package com.luv2code.spring_boot_library.service;
 
 import com.luv2code.spring_boot_library.dao.MessageRepository;
 import com.luv2code.spring_boot_library.entity.Message;
+import com.luv2code.spring_boot_library.requestmodels.AdminQuestionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -20,6 +23,17 @@ public class MessagesService {
         message.setUserEmail(userEmail);
         messageRepository.save(message);
 
+    }
+
+    public void putMessage(AdminQuestionRequest adminQuestionRequest,String userEmail) throws Exception{
+        Optional<Message> message = messageRepository.findById(adminQuestionRequest.getId());
+        if(!message.isPresent()){
+            throw new Exception("message not found");
+        }
+        message.get().setAdminEmail(userEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+        messageRepository.save(message.get());
     }
 
 
